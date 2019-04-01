2224a960a52ba2422083bf67f5f266c879c2dbc5
Chris Y Cho <chrisycho04@gmail.com>
2019-03-31 17:15:51 -0700

    3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/', express.static(path.join(__dirname, '/client/build')));
 
 //ROUTES (uses CONTROLLERS AND MODELS)
 require('./routes/board.routes')(app);
